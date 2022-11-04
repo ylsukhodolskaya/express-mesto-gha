33c14c94d6bfa022b376928e8447e99fcfd7c251
Yuliya Sukhodolskaya <ylsukhodolskaya@yandex.ru>
2022-11-04 20:34:51 +0300

    cards likes delete

--- controllers/cards.js
+++ controllers/cards.js
@@ -10,7 +10,7 @@ export const createCard = (req, res) => {
       if (err.name === 'ValidationError') {
         res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: 'Введены некорректные данные' });
       } else {
-        res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка сервера' });
+        res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка на сервере' });
       }
     });
 };
@@ -20,6 +20,71 @@ export const findCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
     .catch(() => {
-      res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка выгрузки карточек с сервера' });
+      res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка загрузки карточек с сервера' });
+    });
+};
+
+// DELETE-запрос на удаление карточки по id
+export const deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.id)
+    .then((card) => {
+      if (card) {
+        res.send(card);
+      } else {
+        res.status(constants.HTTP_STATUS_NOT_FOUND).send({ message: 'Карточка не найдена' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: 'Введены некорректные данные' });
+      } else {
+        res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка удаления карточки с сервера' });
+      }
+    });
+};
+
+// PUT-запрос постановки лайка
+export const likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => {
+      if (card) {
+        res.send(card);
+      } else {
+        res.status(constants.HTTP_STATUS_NOT_FOUND).send({ message: 'Карточка не найдена' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: 'Введены некорректные данные' });
+      } else {
+        res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка на сервере' });
+      }
+    });
+};
+
+// DELETE-запрос удаления лайка
+export const dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // удалить _id из массива
+    { new: true },
+  )
+    .then((card) => {
+      if (card) {
+        res.send(card);
+      } else {
+        res.status(constants.HTTP_STATUS_NOT_FOUND).send({ message: 'Карточка не найдена' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: 'Введены некорректные данные' });
+      } else {
+        res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка на сервере' });
+      }
     });
 };
--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import {
-  createCard, findCards,
+  createCard,
+  findCards,
+  deleteCard,
+  likeCard,
+  dislikeCard,
 } from '../controllers/cards.js';
 
 export const cardRoutes = Router();
 
 cardRoutes.post('/cards', createCard);
 cardRoutes.get('/cards', findCards);
+cardRoutes.delete('/cards/:id', deleteCard);
+cardRoutes.put('/cards/:cardId/likes', likeCard);
+cardRoutes.delete('/cards/:cardId/likes', dislikeCard);
