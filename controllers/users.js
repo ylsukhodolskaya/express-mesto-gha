cec152127008eee1b88200ccf69e387855363964
Yuliya Sukhodolskaya <ylsukhodolskaya@yandex.ru>
2022-11-04 18:10:35 +0300

    routes user

---
+++ controllers/users.js
@@ -0,0 +1,84 @@
+import { constants } from 'http2';
+import { User } from '../models/user.js';
+
+// POST-запрос для создания нового пользователя
+export const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: 'Введены некорректные данные' });
+      } else {
+        res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка сервера' });
+      }
+    });
+};
+
+// GET-запрос всех пользователей
+export const findUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => {
+      res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка сервера' });
+    });
+};
+
+// GET-запрос по id пользователя
+export const findUserById = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+      } else {
+        res.status(constants.HTTP_STATUS_NOT_FOUND).send({ message: 'Пользователь не найден' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: 'Введены некорректные данные поиска' });
+      } else {
+        res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка сервера' });
+      }
+    });
+};
+
+// PATCH-запрос по обновлению профиля
+export const updateUserProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+    .then((user) => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(constants.HTTP_STATUS_NOT_FOUND).send({ message: 'Пользователь не найден' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: 'Введены некорректные данные при обновлении профиля' });
+      } else {
+        res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка сервера' });
+      }
+    });
+};
+
+// PATCH-запрос по обновлению аватара профиля
+export const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .then((user) => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(constants.HTTP_STATUS_NOT_FOUND).send({ message: 'Пользователь не найден' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: 'Введены некорректные данные при обновлении аватара' });
+      } else {
+        res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка сервера' });
+      }
+    });
+};
---
+++ routes/users.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import {
+  createUser,
+  findUsers,
+  findUserById,
+  updateUserProfile,
+  updateUserAvatar,
+} from '../controllers/users.js';
+
+export const userRoutes = Router();
+
+userRoutes.post('/users', createUser);
+userRoutes.get('/users', findUsers);
+userRoutes.get('/users/:id', findUserById);
+userRoutes.patch('/users/me', updateUserProfile);
+userRoutes.patch('/users/me/avatar', updateUserAvatar);
